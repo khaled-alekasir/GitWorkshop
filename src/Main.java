2b26a7b7083b9f5481fa05c133974c8162ef76cd
khaled <aidinashjari@gmail.com>
2021-06-17 17:18:04 +0430

    the third commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     static public void main(String[] args){
         System.out.println("Hello there!");
         System.out.println("hello!");
+        System.out.println(121);
     }
 }
