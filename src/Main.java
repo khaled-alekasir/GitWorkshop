33faf28c22f300fb906cd9964773c6b0cdab0a5f
khaled <aidinashjari@gmail.com>
2021-06-19 10:10:01 +0430

    the first commit after using "push" command

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello there!");
         System.out.println("hello!");
         System.out.println(121);
+        System.out.println("Used Push command for the first time");
     }
 }
